682f88ef78e5fe8853c0399812bffc17f771bc40
Franklin Cabrera <jamess166@hotmail.com>
2022-07-14 05:10:27 -0500

    Clase de Hooks

---
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react"
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username : 'jamess166',
+        email: 'james@frataingenieros.com',
+    });
+
+    const { username, email } = formState;
+
+    const onInputChange = ( { target } ) => {
+        const { name, value } = target;
+
+        setFormState({
+            ...formState,
+            [name]: value,           
+        })
+    }
+
+
+    useEffect( () => {
+        console.log('useEffect!')
+    });
+
+  return (
+    <>
+        <h1>SimpleForm</h1>
+        <hr />
+
+        <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            name="username"
+            value={ username }
+            onChange={onInputChange}
+        />
+
+        <input
+            type="email"
+            className="form-control mt-2"
+            placeholder="jamescabrerabr@gmail.com"
+            name="email"
+            value={ email }
+            onChange={onInputChange}
+
+        />
+
+    </>
+  )
+}
